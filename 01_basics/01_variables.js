a57415a0925f7e206cc6734d651700ed01032423
Ranjan Kumar <157029372+RanjanKumarBharti@users.noreply.github.com>
2024-09-07 14:46:54 +0000

    Data type done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "merdfkjdfk@gmail.com"
+var accountPassword = "1233445"
+accountCity ="Buxar";
+
+let accountState;
+
+// accountId=2  // not allowed
+
+accountEmail = "jjw@gmail.com"
+accountPassword ="2233444";
+accountCity="bakah";
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+let name="Ranjan Kumar"
+let age=19;
+let isLoggedIn=false
+let StaticRange;
+
+
+//number => 2 to power 53;
+//bigint
+//string=>""
+// Boolean => standalone value
+//undefined=>
+//symbol =>unique
+
+
+//object 
+
+
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
