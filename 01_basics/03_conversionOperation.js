d24d139553d0d9bbdaf4b3d712128be724af6949
Ranjan Kumar <157029372+RanjanKumarBharti@users.noreply.github.com>
2024-09-07 18:14:04 +0000

    conversion is done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,11 +3,11 @@ let score1 = null
 let score2 = "33abc"
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 //"33"=>33
 //"33abc"=>NaN
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isLoggedIn =""
 
 let BooleanIsLoogedIn = Boolean(isLoggedIn)
-console.log(BooleanIsLoogedIn)
+// console.log(BooleanIsLoogedIn)
 
 //1=>true; 0=>false
 
@@ -25,5 +25,34 @@ console.log(BooleanIsLoogedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************** Operations *****************/
+
+let str1 = "Hello"
+let str2 = " Ranjan"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
