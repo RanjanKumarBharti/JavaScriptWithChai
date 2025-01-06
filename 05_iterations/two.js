15a9b7fb42631269ebbf890ecf45536eba826ccf
Ranjan Kumar <157029372+RanjanKumarBharti@users.noreply.github.com>
2025-01-06 13:28:09 +0000

    Done

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// let myArray = ["ironman","superman","spiderman"]
+// let arr=0
+
+// while(arr<myArray.length){
+//     console.log(`Value is :${myArray[arr]}`)
+//     arr = arr+1;
+// }
+
+let score  = 11
+
+do {
+    console.log(`value is ${score}`);
+    score++;  
+} while (score<=10);
